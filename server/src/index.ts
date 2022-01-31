0f2691b3f8a19dc257c4e2a3af6aee3e0c7b4f76
Jerry Xu <jerryxwang9@gmail.com>
2022-01-30 20:38:17 -0800

    Added Express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,23 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   /* MikroORM setup */
   const orm = await MikroORM.init(microConfig); // connect to the database
   await orm.getMigrator().up(); // runs migrations up to the latest
 
-  //   const posts = await orm.em.find(Post, {});
-  //   console.log(posts);
+  /* Express setup */
+  const app = express();
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+
+  // just testing:
+  app.get("/", (req, res) => {
+    res.send("hi welcome to my app");
+  });
 };
 
 main().catch((err) => {
